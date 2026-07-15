f8f01291d010eadae0e17281c975b817b666b024
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-07-15 23:26:22 +0530

    Change background color on button click
    Change background color on button click

--- two/app.js
+++ two/app.js
@@ -1,1 +1,8 @@
+// Question 2 — Change Background Color
 
+const btn = document.querySelector("#btn");
+const body = document.body;
+
+btn.addEventListener("click", () => {
+  body.style.background = "lightblue";
+});
